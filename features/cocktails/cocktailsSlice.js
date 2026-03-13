e76a307bed59c0889921cbe34643b83938311738
lina98st <156576799+lina98st@users.noreply.github.com>
2026-03-13 22:53:40 +0100

    Add favorites slice

---
+++ features/cocktails/cocktailsSlice.js
@@ -0,0 +1,7 @@
+import { createsSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+fetch const fetchCocktail = createAsyncThink(
+    async () => {
+        const response = await fetch(baseUrl) 
+    }
+)
---
+++ features/favorites/favoritesSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const favoritesSlice = createSlice({
+    name: 'favorites',
+    initialState: [],
+    reducers: {
+        addFavorite: (state, action) => {
+            state.push(action.payload);
+        },
+        removeFavorite: (state, action) => {
+            return state.filter(
+             (cocktail) => cocktail.idDrink !== action.payload.idDrink
+            );
+        },
+    },
+});
+
+export const { addFavorite, removeFavorite } = favoritesSlice.actions;
+export default favoritesSlice.reducer;
