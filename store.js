8914db17a7c3d42d4ed0595a85aa84a7664153c1
lina98st <156576799+lina98st@users.noreply.github.com>
2026-03-14 16:07:33 +0100

    Update store with cocktails reducer

--- store.js
+++ store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import favoritesReducer from './features/favorites/favoritesSlice';
+import { cocktailsReducer } from './features/cocktails/cocktailsSlice';
 
 export const store = configureStore({
     reducer: {
         favorites: favoritesReducer,
+        cocktails: cocktailsReducer,
     },
 });
