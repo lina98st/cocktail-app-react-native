92497c5929c1a63ab498845ff29b8422bdb26a29
lina98st <156576799+lina98st@users.noreply.github.com>
2026-03-15 18:42:18 +0100

    Add HomeNavigator to MainComponent

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -2,13 +2,35 @@ import { Image, Platform, StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import FavoritesScreen from './FavoritesScreen';
+import CocktailDetailScreen from './CocktailDetailScreen';
+import { fetchInitialCocktails } from '../features/cocktails/cocktailsSlice';
 
 
 const ButtomTabNav = createBottomTabNavigator();
 
 const HomeNavigator = () => {
+    const Stack = createStackNavigator();
 
-}
+    return (
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{ title: 'Cocktails' }}
+            />
+            <Stack.Screen
+                name='CocktailDetail'
+                component={CocktailDetailScreen}
+                options={({ route }) => ({
+                    title: route.params.cocktail.strDrink
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
 
 const FavoritesNavigator = () => {
 
